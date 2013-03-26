d6a3a7ae54fb10e9711a3d9ba3b3beb66ef1b3ea
Alberto Mijares <almilo@almilo.es>
2013-03-26 17:09:16 +0100

    initial application

---
+++ transport.js
@@ -0,0 +1,53 @@
+angular.module("Transport", []);
+
+function TransportCtrl($scope, $http) {
+    var LOCATIONS_SERVICE = "http://transport.opendata.ch/v1/locations";
+
+    $scope.doSearch = function (stationName) {
+        if (stationName === undefined) {
+            stationName = $scope.stationName;
+        } else {
+            $scope.stationName = stationName;
+        }
+
+        var query = "*" + stationName + "*";
+        $http.get(
+            LOCATIONS_SERVICE,
+            {params: {query: query}}
+        ).success(function (data) {
+                $scope.locationsResult = data;
+            });
+    };
+
+    function setPlaceholder(value) {
+        $("#stationName").attr("placeholder", value);
+    }
+
+    function geoLocate() {
+        if (navigator.geolocation) {
+            setPlaceholder("Locating...");
+
+            navigator.geolocation.getCurrentPosition(function (position) {
+                    var x = position.coords.latitude;
+                    var y = position.coords.longitude;
+
+                    $http.get(
+                        LOCATIONS_SERVICE,
+                        {params: {x: x, y: y}}
+                    ).success(function (data) {
+                            var stationName = data.stations.length > 0 ? data.stations[0].name : "";
+                            $scope.doSearch(stationName);
+                        }).error(function(data, status) {
+                            $scope.locationsResult = {stations: [{name: "Error code: '" + status + "' retrieving locations."}]};
+                        });
+
+                    setPlaceholder("");
+                },
+                function () {
+                    setPlaceholder("");
+                });
+        }
+    }
+
+    geoLocate();
+}
