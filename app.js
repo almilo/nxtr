6a8892bcb418e682f101a2d0f483f7be0b9778aa
Alberto Mijares <almilo@almilo.es>
2013-03-28 13:42:50 +0100

    reorganized files for testing added karma config file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var PORT = process.env.PORT || 8080;
 var express = require('express');
 var app = express.createServer();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/public"));
 
 app.listen(PORT);
 
